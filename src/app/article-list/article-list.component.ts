e98fb12a5fa7f312c729665f843ff8a72a2bd3be
Girlhack007 <tourendeyeyande@gmail.com>
2024-08-02 16:18:15 +0000

    Stylisation et ajout de l'id dans la modification pour le récupérer et  modifier et afficher le dernier article ajouté en haut de l'application

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,91 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../article.service';
-// Permet d'utiliser les ngfor et ngif etc
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 import { Article } from '../article.model';
 
 @Component({
- selector: 'app-article-list',
- standalone: true,
- imports: [CommonModule, RouterLink, ReactiveFormsModule],
- templateUrl: './article-list.component.html',
- styleUrl: './article-list.component.css'
+  selector: 'app-article-list',
+  standalone: true,
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.css']
 })
 export class ArticleListComponent implements OnInit {
-articleForm: FormGroup;
- articles: Article[] = [];
+  articleForm: FormGroup;
+  articles: Article[] = [];
 
- constructor(
-  private articleService: ArticleService,
-  private fb: FormBuilder
+  constructor(
+    private articleService: ArticleService,
+    private fb: FormBuilder
   ) { 
+    // Initialisation du formulaire avec les champs id, title et body
     this.articleForm = this.fb.group({
+      id: [null],  // Champ id ajouté pour gérer la modification
       title: ['', Validators.required],
       body: ['', Validators.required]
     });
   }
 
-// OnSubmit for add and update
-onSubmit() {
-  const article = this.articleForm.value;
-  if (article.id) {
-    this.articleService.updatePost(article.id, article).subscribe(() => {
-      const index = this.articles.findIndex((p) => p.id === article.id);
-      this.articles[index] = article  // For demonstration, update locally
-      Swal.fire('Succès', 'Article modifié avec succès', 'success');
-    });
-  } else {
-    this.articleService.createPost(article).subscribe(() => {
-      this.articles.unshift(article)  // For demonstration, prepend locally
-      Swal.fire('Succès', 'Article ajouté avec succès', 'success')
+  // Méthode onSubmit pour ajouter ou mettre à jour un article
+  onSubmit() {
+    const article = this.articleForm.value;
+
+    if (article.id) {
+      // Si l'id est présent, on met à jour l'article existant
+      this.articleService.updatePost(article.id, article).subscribe(() => {
+        const index = this.articles.findIndex((p) => p.id === article.id);
+        this.articles[index] = { ...this.articles[index], ...article };  // Mise à jour locale de l'article
+        Swal.fire('Succès', 'Article modifié avec succès', 'success');
+      });
+    } else {
+      // Si l'id n'est pas présent, on ajoute un nouvel article
+      this.articleService.createPost(article).subscribe((newArticle) => {
+        this.articles.unshift(newArticle);  // Ajout local de l'article en tête de liste
+        Swal.fire('Succès', 'Article ajouté avec succès', 'success');
+      });
+    }
+
+    this.articleForm.reset();  // Réinitialisation du formulaire après soumission
+  }
+
+  // Méthode pour préparer le formulaire à la modification d'un article
+  editArticle(article: Article) {
+    this.articleForm.patchValue({
+      id: article.id,
+      title: article.title,
+      body: article.body
     });
   }
-  this.articleForm.reset();
-}
 
-editArticle(article:Article){
-  this.articleForm.patchValue(article)
-}
+  // Méthode ngOnInit pour charger les articles au démarrage
+  ngOnInit() {
+    this.chargerArticles();
+  }
 
- ngOnInit() {
-   this.chargerArticles();
- }
+  // Charger les articles depuis le service
+  chargerArticles() {
+    this.articleService.getPosts().subscribe((articles) => {
+      this.articles = articles.reverse();  // Inversion pour avoir les plus récents en premier
+    });
+  }
 
- // Charger les articles
- chargerArticles() {
-   this.articleService.getPosts().subscribe((article) => {
-     this.articles = article;
-   });
- }
-
-
- // Ajouter un article
-//  ajouterArticle() {
-//    const nouvelArticle = { title: 'New Post', body: 'This is a new post.', userId: 1 };
-//    this.articleService.createPost(nouvelArticle).subscribe(() => {
-//      this.articles.unshift(nouvelArticle);  // For demonstration, prepend locally
-//    });
-//  }
-
-
- // Modifier un article
-//  modifierArticle(article: any) {
-//    const articleModifie = { ...article, title: 'Updated Title' };
-//    this.articleService.updatePost(article.id, articleModifie).subscribe(() => {
-//      const index = this.articles.findIndex((p) => p.id === article.id);
-//      this.articles[index] = articleModifie;  // For demonstration, update locally
-//    });
-//  }
-
-// Supprimer un article
- supprimerArticle(id: number) {
-  this.articleService.deletePost(id).subscribe(() => {
-    this.articles = this.articles.filter((article) => article.id !== id);
-    Swal.fire('Succès', 'Article supprimé avec succès', 'success')
-  });
-}
-
+  // Méthode pour supprimer un article
+  supprimerArticle(id: number) {
+    this.articleService.deletePost(id).subscribe(() => {
+      this.articles = this.articles.filter((article) => article.id !== id);
+      Swal.fire('Succès', 'Article supprimé avec succès', 'success');
+    });
+  }
 }
