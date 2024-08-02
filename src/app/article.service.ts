ff0b3934cefcf06f3f298123bc1dd5c2a7db9357
Girlhack007 <tourendeyeyande@gmail.com>
2024-08-02 15:24:50 +0000

    Ajout de l'Url de mon Api

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs'; //Pour les observable
   providedIn: 'root'
 })
 export class ArticleService {
-  private apiUrl = 'https://jsonplaceholder.typicode.com/posts'; //Cette URL nous permet de récupérer les posts
+  private apiUrl = 'http://127.0.0.1:8000/api/articles'; //Cette URL nous permet de récupérer les posts
   private commentsUrl = 'https://jsonplaceholder.typicode.com/comments'; //Cette URL nous permet de récupérer les Commentaires
   //Dans un service, on déclare les services qu'on utilise dans notre application. Ici, on déclare un service pour récupérer les posts.
   constructor(private http:HttpClient)  { 
@@ -22,12 +22,12 @@ export class ArticleService {
   }
 
   //Méthode créer un post 
-  createPost(postData: { title: string; body: string; userId: number }): Observable<any> {
+  createPost(postData: { title: string; body: string}): Observable<any> {
     // Ici on utilise post pour ajouter
     return this.http.post(this.apiUrl, postData);
   }
 
-  // Méthide pour mettre à jour un post en récupérant l'id du post typé et les données du post typées également
+  // Méthode pour mettre à jour un post en récupérant l'id du post typé et les données du post typées également
   updatePost(id: number, postData: { title: string; body: string; userId: number }): Observable<any> {
     // Ici on utilise put pour modifier le post
     return this.http.put(`${this.apiUrl}/${id}`, postData);
@@ -42,5 +42,4 @@ export class ArticleService {
     getComments(id:number): Observable<any> {
       return this.http.get(`${this.commentsUrl}?postId=${id}`);
     }
-  
 }
